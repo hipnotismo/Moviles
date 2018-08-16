8934be802a6d1f8b93ccf6c369a7e0862893a9d2
Unknown <agustingarcia1997@gmail.com>
2018-08-15 23:40:11 -0300

    MVP comenzado, Player y enemigos funcionamiento basico

---
+++ Mobiles MVP/Assets/Scripts/GameData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour {
+
+    [SerializeField] private GameObject player;
+    private Vector3 plyPos;
+
+    public Vector3 PlyPos
+    {
+        get
+        {
+            return plyPos;
+        }
+    }
+
+    void Awake ()
+    {
+        plyPos = player.transform.position;
+	}
+
+    private void Update()
+    {
+        if (plyPos != player.transform.position)
+            plyPos = player.transform.position;
+    }
+}
---
+++ Mobiles MVP/Assets/Scripts/PlyShoot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlyShoot : MonoBehaviour {
+	
+	void Update ()
+    {
+        bool shooting = Input.GetKeyDown(KeyCode.Mouse0);
+        RaycastHit hitInfo;
+        Physics.Raycast(transform.position, transform.forward, out hitInfo);
+        Debug.DrawRay(transform.position, transform.forward * 100, Color.red);
+
+        if (shooting)
+        {
+            if (hitInfo.transform.CompareTag("Enemy"))
+            {
+                Destroy(hitInfo.transform.gameObject);
+            }
+        }
+	}
+}
