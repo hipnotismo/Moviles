0654611d815ed7fc97274027b419b4459ac4b42e
Unknown <agustingarcia1997@gmail.com>
2018-08-28 17:55:49 -0300

    Cambios minimos

---
+++ Mobiles MVP/Assets/Scripts/BlockBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockBehaviour : MonoBehaviour {
+
+    [SerializeField] GameObject[] barrs;
+    [SerializeField] private float health;
+    float totalHealth;
+    int pos;
+
+    private void Awake()
+    {
+        totalHealth = health;
+    }
+
+    void Update ()
+    {
+        if (health <= totalHealth - totalHealth / 3)
+        {
+            barrs[pos].SetActive(false);
+            pos++;
+            totalHealth -= totalHealth / 3;
+        }
+	}
+}
---
+++ Mobiles MVP/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] private float range;
+
+	void Update ()
+    {
+        RaycastHit hitInfo;
+        bool hit = Physics.Raycast(transform.position, transform.forward, out hitInfo, range);
+
+        if (hit)
+        {
+
+        }
+	}
+}
