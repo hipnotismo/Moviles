f7083309bcdc0978d0c77efc5a3417a03c5562dc
Unknown <agustingarcia1997@gmail.com>
2018-09-10 00:13:49 -0300

    Player dispara a travez de barreras

--- Moviles/Assets/Scripts/PlyShoot.cs
+++ Moviles/Assets/Scripts/PlyShoot.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class PlyShoot : MonoBehaviour {
 
     [SerializeField] private float damage;
+    [SerializeField] private LayerMask isEnemy;
 
-	void Update ()
+    void Update ()
     {
         bool shooting = Input.GetKeyDown(KeyCode.Mouse0);
-        RaycastHit hitInfo;
-        Physics.Raycast(transform.position, transform.forward, out hitInfo);
+        RaycastHit hitInfo; 
+        Physics.Raycast(transform.position, transform.forward, out hitInfo, Mathf.Infinity, isEnemy);
         Debug.DrawRay(transform.position, transform.forward * 100, Color.red);
 
         if (shooting)
